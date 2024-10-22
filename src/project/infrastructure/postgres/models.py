650ecbf80c1b6dce6adcd2e83ca423b52bbec945
Александр Кудряшов <alexandro@MacBook-Air-Aleksandr.local>
2024-10-22 19:02:04 +0400

    Init Project

---
+++ src/project/infrastructure/postgres/models.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+from sqlalchemy import Date
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.project.infrastructure.postgres.database import Base
+
+
+class Clients(Base):
+	__tablename__ = "clients_list"
+
+	id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+	full_name: Mapped[str] = mapped_column(nullable=False)
+	date_of_birth: Mapped[date] = mapped_column(Date, nullable=False)
+	phone_number: Mapped[str] = mapped_column(nullable=False)
+	total_amount_of_work: Mapped[int] = mapped_column(nullable=True)
+
+
+
